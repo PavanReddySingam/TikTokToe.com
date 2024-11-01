20cf09cfcfb5288f0bda35d0cd8e543790021144
Singam Pavan Kumar Reddy <166385151+PavanReddySingam@users.noreply.github.com>
2024-11-01 11:19:13 +0530

    Add files via upload

---
+++ TikTokToe.js
@@ -0,0 +1,17 @@
+let btn=document.getElementsByTagName('button');
+
+function start()
+{
+   while(i<=9)
+    {
+        if(i%2!=0)//person1
+        {
+            
+        }
+        else//person 2
+        {
+            
+        }
+    }
+}
+
